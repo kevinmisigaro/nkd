397777d88f6c327c92e3e2edb6c1664824804c15
Kevin Misigaro <kevin@databalk.nu>
2024-04-05 20:58:16 +0200

    added posthog

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,15 @@
 import Footer from "@/components/Footer";
 import "./globals.css";
 import Navbar from "@/components/NavBar";
+import { posthog } from "posthog-js";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, { api_host: 'https://app.posthog.com' })
+
   return (
     <html lang="en">
       <body>
